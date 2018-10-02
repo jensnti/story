3775d9bef98d9fe9dfc2a4d206bc35aad7426fd3
JensAndreasson <42906632+JensAndreasson@users.noreply.github.com>
2018-10-02 15:58:27 +0200

    Intial files

---
+++ include/dbinfo_example.php
@@ -0,0 +1,5 @@
+<?php
+	$dbuser = "";
+	$dbpass = "";
+	$db = "";
+?>
---
+++ play.php
@@ -0,0 +1,52 @@
+<?php
+	session_start();
+?>
+<!doctype html>
+<html lang="se">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+	<title>Soloäventyr - Spela</title>
+	<link href="https://fonts.googleapis.com/css?family=Merriweather|Merriweather+Sans" rel="stylesheet"> 
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav id="navbar">
+	<a href="index.php">Hem</a>
+	<a class="active" href="play.php">Spela</a>
+	<a href="edit.php">Redigera</a>
+</nav>	
+<main class="content">
+	<section>
+		<h1>Spela</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit hic aliquid nostrum quibusdam veritatis? Eaque accusantium odit id deserunt, quae minima adipisci nesciunt illum ipsa ea placeat, earum laboriosam corrupti.</p>
+		<footer class="gotopagelinks">
+			<p>
+				<a href="play.php?page=1">Nästa sida</a>
+				<a href="play.php?page=2">Gå till sidan</a>
+			</p>
+		</footer>
+<?php
+	include_once 'include/dbinfo.php';
+
+	// PDO
+
+	if (isset($_GET['page'])) {
+		// TODO load requested page from DB using GET
+		// prio before session
+		// set session to remember
+		$filteredPage = filter_input(INPUT_GET, "page", FILTER_VALIDATE_INT);
+
+		echo "<p>Requested page " . $filteredPage . "</p>";
+	} elseif(isset($_SESSION['page'])) {
+		// TODO load page from db
+		// use for returning player / cookie
+	} else {
+		// TODO load start of story from DB
+	}
+
+?>
+</main>
+<script src="navbar.js"></script>
+</body>
+</html>
